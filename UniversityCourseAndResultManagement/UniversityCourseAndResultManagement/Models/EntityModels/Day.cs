4773d1e0522e2671b2e2b26d3484e078e20eebc5
AnowerRony <hossainanower4@gmail.com>
2017-10-22 11:02:59 +0600

    Day Model

---
+++ UniversityCourseAndResultManagement/UniversityCourseAndResultManagement/Models/EntityModels/Day.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCourseAndResultManagement.Models.EntityModels
+{
+    public class Day
+    {
+        public int Id { get; set; }
+        public string DayName { get; set; }
+    }
+}
