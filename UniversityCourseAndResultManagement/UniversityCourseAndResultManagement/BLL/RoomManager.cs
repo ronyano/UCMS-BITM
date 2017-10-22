1ce687807014190ec29a046f96279a7743cb4b98
AnowerRony <hossainanower4@gmail.com>
2017-10-22 11:25:55 +0600

    Room Manager

---
+++ UniversityCourseAndResultManagement/UniversityCourseAndResultManagement/BLL/RoomManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCourseAndResultManagement.DAL;
+using UniversityCourseAndResultManagement.Models.EntityModels;
+
+namespace UniversityCourseAndResultManagement.BLL
+{
+    public class RoomManager
+    {
+        RoomGateway roomGateway = new RoomGateway();
+
+        public List<Room> GetAllRooms()
+        {
+            return roomGateway.GetAllRooms();
+        }
+    }
+}
