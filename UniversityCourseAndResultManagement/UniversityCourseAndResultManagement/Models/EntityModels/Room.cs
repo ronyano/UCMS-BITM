b74e3ecfc586a078a96920d70843dbb7689c75fd
AnowerRony <hossainanower4@gmail.com>
2017-10-22 11:05:29 +0600

    Room Model

---
+++ UniversityCourseAndResultManagement/UniversityCourseAndResultManagement/Models/EntityModels/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCourseAndResultManagement.Models.EntityModels
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int RoomNo { get; set; }
+    }
+}
