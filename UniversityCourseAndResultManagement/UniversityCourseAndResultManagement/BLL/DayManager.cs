1905fa0dfe383e82204562d163f7ea64daefe20b
AnowerRony <hossainanower4@gmail.com>
2017-10-22 11:22:54 +0600

    Day Manager

---
+++ UniversityCourseAndResultManagement/UniversityCourseAndResultManagement/BLL/DayManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCourseAndResultManagement.DAL;
+using UniversityCourseAndResultManagement.Models.EntityModels;
+
+namespace UniversityCourseAndResultManagement.BLL
+{
+    public class DayManager
+    {
+        DayGateway dayGateway = new DayGateway();
+
+        public List<Day> GetAllDays()
+        {
+            return dayGateway.GetAllDays();
+        }
+    }
+}
